bc05dad9225b4d3c4a4b77f1b6b8949e32803420
bob chase <bob.chase@gmail.com>
2017-12-28 10:58:35 -0500

    add nested_get

---
+++ test/test_nested_get.py
@@ -0,0 +1,50 @@
+import pytest
+
+from tools.nested_get import nested_get
+
+
+@pytest.fixture
+def data():
+    return {'a': {'b': {'c': 10}}}
+
+
+@pytest.mark.parametrize('keys,expected', [
+    (('a',), {'b': {'c': 10}}),
+    (('a', 'b'), {'c': 10}),
+    (('a', 'b', 'c'), 10),
+    (('a', 'b', 'c', 'd'), None),
+])
+def test_basic(data, keys, expected):
+    assert nested_get(data, keys) == expected
+
+
+@pytest.mark.parametrize('keys,expected', [
+    (('a',), {'b': {'c': 10}}),
+    (('a', 'b'), {'c': 10}),
+    (('a', 'b', 'c'), 10),
+    (('a', 'b', 'c', 'd'), 'hey'),
+])
+def test_default(data, keys, expected):
+    assert nested_get(data, keys, default='hey') == expected
+
+
+def test_required_keyerror(data):
+    with pytest.raises(KeyError):
+        assert nested_get(data, ('z',), required=True)
+
+
+def test_required_typeerror(data):
+    with pytest.raises(TypeError):
+        assert nested_get(data, ('a', 'b', 'c', 'd'), required=True)
+
+
+def test_list(data):
+    assert nested_get(data, ('a', 'b', 'c'), as_list=True) == [10]
+    assert nested_get(data, ('a', 'b'), as_list=True) == [{'c': 10}]
+
+
+def test_list_default(data):
+    assert nested_get(data, ('a', 'b', 'c', 'd'), as_list=True) == []
+    assert nested_get(
+        data, ('a', 'b', 'c', 'd'), default='foo', as_list=True
+    ) == ['foo']
---
+++ tools/nested_get.py
@@ -0,0 +1,38 @@
+def nested_get(d, keys, default=None, required=False, as_list=False):
+    """ Multi-level dict get helper
+
+        Parameters:
+
+            d        - dict instance
+            keys     - iterable of keys (see Note 1)
+            default  - value if index fails
+            required - require every index to work (see Note 2)
+            as_list  - return result as list (see Note 3)
+
+        Notes:
+            1. Each key is used to index a dict, replacing the dict with
+               the matching value. This process is repeated until an index
+               fails, or the keys are exhausted.
+            2. If the required flag is False, a failure to match a key
+               causes the default value to be used. If the required flag is
+               True, every key must match, otherwise a TypeError or KeyError is
+               raised.
+            3. If as_list is True, a non-list match will be wrapped in a list,
+               unless match is None, which will be replaced with an empty list.
+    """
+    for key in keys:
+        try:
+            d = d[key]
+        except KeyError:
+            if required:
+                raise
+            d = default
+            break
+        except TypeError:
+            if required:
+                raise
+            d = default
+            break
+    if as_list:
+        return [] if d is None else [d] if not isinstance(d, list) else d
+    return d
