16d5269923b0ab9f02b51b3e5226e3122a9a17aa
bob chase <bob.chase@gmail.com>
2018-01-31 07:01:37 -0500

    improved defaults for normalize_path

--- ergaleia/normalize_path.py
+++ ergaleia/normalize_path.py
@@ -14,7 +14,7 @@ else:
     import imp
 
 
-def normalize_path(path, filetype=None):
+def normalize_path(path, filetype=None, has_filetype=True):
     """ Convert dot-separated paths to directory paths
 
     Allows non-python files to be placed in the PYTHONPATH and be referenced
@@ -23,14 +23,15 @@ def normalize_path(path, filetype=None):
     If a text file, named test.txt was placed in a python repo named myprog in
     the module named util, then:
 
-        normalize_path('myprog.util.test.txt', 'txt')
+        normalize_path('myprog.util.test.txt')
 
-    would return the file-system's path to the file.
+    would return the file-system's path to the file 'test.txt'.
 
     Parameters:
 
-        path - path to convert
-        filetype - don't include as part of path if present as last token
+        path         - path to convert
+        filetype     - don't include as part of path if present as last token
+        has_filetype - if True, treat last dot-delimited token as filetype
 
     Notes:
         1. Paths are relative to PYTHONPATH.
@@ -38,21 +39,27 @@ def normalize_path(path, filetype=None):
            change.
         3. If the specified path contains os-specific path separator
            characters, the path is returned without change.
+        4. If has_filetype is True, filetype does not have to be specified.
+           If filetype is specified, has_filetype is ignored, and filetype
+           must match the last dot-delimited token exactly.
     """
     if not isinstance(path, str):
         return path
     if '.' in path and os.path.sep not in path:  # path is dot separated
         parts = path.split('.')
         extension = ''
-        if filetype and parts[-1] == filetype:
-            parts = parts[:-1]  # save '.filetype' extension if exists
-            extension = '.' + filetype
         if len(parts) > 1:
-            if PY3:
-                spec = importlib.util.find_spec(parts[0])
-                path = spec.submodule_search_locations[0]
-            else:
-                _, path, _ = imp.find_module(parts[0])
+            if filetype and has_filetype:
+                has_filetype = False  # filetype is more specific
+            if (filetype and parts[-1] == filetype) or has_filetype:
+                extension = '.' + parts[-1]
+                parts = parts[:-1]
+            if len(parts) > 1:
+                if PY3:
+                    spec = importlib.util.find_spec(parts[0])
+                    path = spec.submodule_search_locations[0]
+                else:
+                    _, path, _ = imp.find_module(parts[0])
 
-            path = os.path.join(path, *parts[1:]) + extension
+                path = os.path.join(path, *parts[1:]) + extension
     return path
--- test/test_normalize_path.py
+++ test/test_normalize_path.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 from ergaleia.normalize_path import normalize_path
 
@@ -6,7 +7,15 @@ from ergaleia.normalize_path import normalize_path
 BASE = os.path.sep.join(__file__.split(os.path.sep)[:-2])
 
 
-def test_normalize_path():
+@pytest.mark.parametrize('path,filetype,has_filetype,expected', [
+    ('test.b.c.d', None, True, 'test/b/c.d'),
+])
+def test_normalize_path(path, filetype, has_filetype, expected):
+    assert normalize_path(path, filetype, has_filetype) == \
+        os.path.sep.join((BASE, expected))
+
+
+def test_normalize_path_with_filetype():
     assert normalize_path('test.load_from_path.data', 'data') == \
         os.path.sep.join((BASE, 'test/load_from_path.data'))
 
