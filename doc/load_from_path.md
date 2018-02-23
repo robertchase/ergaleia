7ba662b5463b047a4096c0d53fb9000ba9bed3c0
bob chase <bob.chase@gmail.com>
2018-02-23 07:01:09 -0500

    add has_filetype to load_from_path

--- doc/load_from_path.md
+++ doc/load_from_path.md
@@ -1,1 +1,28 @@
-# load_from_file
+# load_from_path
+
+Find and load files in pythonic ways.
+
+## usage
+```
+load_from_path(path, filetype=None, has_filetype=True)
+
+    Parameters: (see normalize_path)
+        path         - dot-separated path
+        filetype     - optional filetype
+        has_filetype - if True, treat last dot-delimited token as filetype
+
+    Notes:
+        1. If path is a file-like object, then data is read directly
+           from path, without trying to open it.
+        2. Non-string paths are returned immediately (excluding the
+           case in Note 1).
+        3. If has_filetype is True, filetype does not have to be specified.
+           If filetype is specified, has_filetype is ignored, and filetype
+           must match the last dot-delimited token exactly.
+```
+
+## example
+
+```
+>>> from ergaleia.load_from_path import load_from_path
+```
--- ergaleia/load_from_path.py
+++ ergaleia/load_from_path.py
@@ -6,49 +6,55 @@ https://github.com/robertchase/ergaleia/blob/master/LICENSE.txt
 from ergaleia.normalize_path import normalize_path
 
 
-def load_from_path(path, filetype=None):
+def load_from_path(path, filetype=None, has_filetype=True):
     """ load file content from a file specified as dot-separated
 
         The file is located according to logic in normalize_path,
         and the contents are returned. (See Note 1)
 
-        Parameters:
-
-            path - dot-separated path (see normalize_path)
-            filetype - optional filetype (see normalize_path)
+        Parameters: (see normalize_path)
+            path         - dot-separated path
+            filetype     - optional filetype
+            has_filetype - if True, treat last dot-delimited token as filetype
 
         Notes:
             1. If path is a file-like object, then data is read directly
                from path, without trying to open it.
             2. Non-string paths are returned immediately (excluding the
                case in Note 1).
+            3. If has_filetype is True, filetype does not have to be specified.
+               If filetype is specified, has_filetype is ignored, and filetype
+               must match the last dot-delimited token exactly.
     """
     if not isinstance(path, str):
         try:
             return path.read()
         except AttributeError:
             return path
-    path = normalize_path(path, filetype)
+    path = normalize_path(path, filetype, has_filetype)
     with open(path) as data:
         return data.read()
 
 
-def load_lines_from_path(path, filetype=None):
+def load_lines_from_path(path, filetype=None, has_filetype=True):
     """ load lines from a file specified as dot-separated
 
         The file is located according to logic in normalize_path,
         and a list of lines is returned. (See Note 1)
 
-        Parameters:
-
-            path - dot-separated path (see normalize_path)
-            filetype - optional filetype (see normalize_path)
+        Parameters: (see normalize_path)
+            path         - dot-separated path
+            filetype     - optional filetype
+            has_filetype - if True, treat last dot-delimited token as filetype
 
         Notes:
-            1. If path is a file-like object, then data is read directly
+            1. If path is a file-like object, then lines are read directly
                from path, without trying to open it.
             2. Non-string paths are returned immediately (excluding the
                case in Note 1).
+            3. If has_filetype is True, filetype does not have to be specified.
+               If filetype is specified, has_filetype is ignored, and filetype
+               must match the last dot-delimited token exactly.
     """
     if not isinstance(path, str):
         try:
