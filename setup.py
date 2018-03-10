62b1edfad3df3303f9c93db43bc69e5902b40edb
bob chase <bob.chase@gmail.com>
2018-03-10 14:17:26 -0500

    prepare for pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
+from setuptools import find_packages
 from distutils.core import setup
+
 setup(
     name='ergaleia',
-    version='1.1',
-    packages=[
-        'ergaleia',
-    ],
+    version='1.2.0rc1',
+    packages=find_packages(exclude=['tests']),
+    description='A library of random tools',
+    long_description="""
+Documentation
+-------------
+    You can see the project and documentation at the `GitHub repo <https://github.com/robertchase/ergaleia>`_
+    """,
+    author='Bob Chase',
+    url='https://github.com/robertchase/ergaleia',
+    license='MIT',
 )
