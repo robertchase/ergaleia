8ab037ef6b4ee8bad5e4dd83078669781eb4ae06
bob chase <bob.chase@gmail.com>
2018-11-04 05:37:06 -0500

    1.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='ergaleia',
-    version='1.3.0',
+    version='1.3.1',
     packages=find_packages(exclude=['tests']),
     description='A library of random tools',
     long_description="""
